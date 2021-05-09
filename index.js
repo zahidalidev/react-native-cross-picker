f2cd5212adf138d7cf4485535f7ab5ffba2b08e6
zahidalidev <m.zahidalidev@gmail.com>
2021-05-09 18:21:07 +0500

    :art:

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { View, Modal, ScrollView, TouchableOpacity, Text, TouchableWithoutFeedback } from 'react-native';
 import { RFPercentage } from 'react-native-responsive-fontsize';
 
-function ReactNativeCrossPicker({ placeHolderColor = "grey", placeHolderSize = RFPercentage(2.9), mainComponentStyle, modalComponentStyle, modalTextStyle, items, setItem, selectedItem, placeholder = "Select Item", width = "100%", iconComponent, modalMarginTop = "50%", modalMaxHeight = RFPercentage(40), modalBottomBorderColor }) {
+function ReactNativeCrossPicker({ placeholderColor = "grey", placeholderSize = RFPercentage(2.2), mainComponentStyle, placeholderStyle, modalComponentStyle, modalTextStyle, items, setItem, selectedItem, placeholder = "Select Item", width = "100%", iconComponent, modalMarginTop = "50%", modalMaxHeight = "35%", modalBottomBorderColor }) {
   const [modalVisible, setModalVisible] = useState(false)
 
   return (
     <>
       <TouchableWithoutFeedback onPress={() => setModalVisible()} >
-        <View style={{ borderColor: "grey", borderWidth: 0.5, padding: RFPercentage(1.4), paddingRight: 0, borderRadius: RFPercentage(1), width, flexDirection: "row", justifyContent: "center", alignItems: "center", ...mainComponentStyle }} >
-          <Text style={{ fontSize: placeHolderSize, color: placeHolderColor, width: "90%" }} >{selectedItem ? selectedItem : placeholder}</Text>
+        <View style={{ borderColor: "grey", borderWidth: 1, padding: RFPercentage(1.4), paddingRight: 0, borderRadius: RFPercentage(1), width, height: RFPercentage(6), flexDirection: "row", justifyContent: "center", alignItems: "center", ...mainComponentStyle }} >
+          <Text style={{ fontSize: placeholderSize, color: placeholderColor, width: "90%", ...placeholderStyle }} >{selectedItem ? selectedItem : placeholder}</Text>
           <View style={{ width: "10%" }} >
             {iconComponent()}
           </View>
@@ -22,7 +22,7 @@ function ReactNativeCrossPicker({ placeHolderColor = "grey", placeHolderSize = R
             <Text style={{ color: "red", fontSize: RFPercentage(2), alignSelf: "flex-end", marginRight: RFPercentage(3) }} >Cancel</Text>
           </TouchableWithoutFeedback>
 
-          <ScrollView contentContainerStyle={{ backgroundColor: "white", justifyContent: "center", ...modalComponentStyle }} style={{ marginTop: RFPercentage(2), width: "90%" }} showsVerticalScrollIndicator={false} >
+          <ScrollView contentContainerStyle={{ backgroundColor: "white", justifyContent: "center", width: "90%", marginLeft: "5%", ...modalComponentStyle }} style={{ marginTop: RFPercentage(2), width: "90%" }} showsVerticalScrollIndicator={false} >
             {items.map((item, index) => <TouchableOpacity style={{ marginTop: RFPercentage(1), justifyContent: "center", alignItems: "flex-start", borderBottomWidth: modalBottomBorderColor ? 0.5 : 0, borderBottomColor: modalBottomBorderColor }} key={index} onPress={() => {
               setModalVisible(false);
               setItem(item.value)
